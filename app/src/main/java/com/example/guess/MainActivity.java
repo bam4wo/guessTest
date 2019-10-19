c015b97ec45d4a4e8f040b4ba7456522ae3ca40c
Bam <wubetty0727@gmail.com>
2019-10-19 11:40:31 +0800

    Test

---
+++ app/src/main/java/com/example/guess/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.guess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    //Random random = new Random();
+    int secret = new Random().nextInt(50)+1;
+    private EditText num;
+    private TextView hint;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println(secret);
+        num = findViewById(R.id.ed_num);
+        hint = findViewById(R.id.te_hint);
+    }
+
+
+    public void guess(View view){
+        int number = Integer.parseInt(num.getText().toString());
+        if (number > secret){
+            hint.setText("Smaller");
+        }else if (number < secret){
+            hint.setText("Bigger" );
+        }else {
+            hint.setText("Bingo" );
+        }
+    }
+}
